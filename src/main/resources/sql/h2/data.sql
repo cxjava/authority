382199a661aac75eda458be22931b169432d07e1
cxjava <cxjava@gmail.com>
2012-11-12 09:56:56 +0800

    update sql data

--- src/main/resources/sql/h2/data.sql
+++ src/main/resources/sql/h2/data.sql
@@ -20,11 +20,11 @@ INSERT INTO t_base_role_module (id,role_id,module_id) VALUES (7,1,11);
 INSERT INTO t_base_role_module (id,role_id,module_id) VALUES (8,1,14);
 INSERT INTO t_base_role_module (id,role_id,module_id) VALUES (9,1,21);
 
-INSERT INTO t_base_user (id,account,password,real_name,sex,email,mobile,office_phone,error_count,last_login_time,last_login_ip,remark) VALUES (1,'test','ddee6e95fae5bb5f8890a6f9ef7d0d1db744ca4417e94c05595ef280046a49021eba3291ee9c9cf8','测试用户',0,'test@qq.com','119','110',0,'2012-11-06 16:52:07',NULL,NULL);
-INSERT INTO t_base_user (id,account,password,real_name,sex,email,mobile,office_phone,error_count,last_login_time,last_login_ip,remark) VALUES (2,'admin','6043ae1095884cf9663d140ee6450b49b8489b3aa073a8eec024492b976ee2a24aee0c272369121b','超级管理员',0,'admin@whty.com.cn','119','110',0,'2012-11-07 15:52:04','127.0.0.1','用户信息');
+INSERT INTO t_base_user (id,account,password,real_name,sex,email,mobile,office_phone,error_count,last_login_time,last_login_ip,remark) VALUES (1,'admin','6043ae1095884cf9663d140ee6450b49b8489b3aa073a8eec024492b976ee2a24aee0c272369121b','超级管理员',0,'admin@qq.com.cn','119','110',0,'2012-11-07 15:52:04','127.0.0.1','用户信息');
+INSERT INTO t_base_user (id,account,password,real_name,sex,email,mobile,office_phone,error_count,last_login_time,last_login_ip,remark) VALUES (2,'test','ddee6e95fae5bb5f8890a6f9ef7d0d1db744ca4417e94c05595ef280046a49021eba3291ee9c9cf8','测试用户',0,'test@qq.com','119','110',0,'2012-11-06 16:52:07',NULL,NULL);
 
-INSERT INTO t_base_user_role (id,user_id,role_id) VALUES  (1,2,1);
-INSERT INTO t_base_user_role (id,user_id,role_id) VALUES  (2,1,2);
+INSERT INTO t_base_user_role (id,user_id,role_id) VALUES  (1,1,1);
+INSERT INTO t_base_user_role (id,user_id,role_id) VALUES  (2,2,2);
  
  
 INSERT INTO t_base_field (id,field,field_name,value_field,display_field,enabled,sort) VALUES (1,'sex','性别','0','男',1,1);
